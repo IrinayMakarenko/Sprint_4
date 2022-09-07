0972456151078e046eeed766e9d4d29aa8adc6fd
Irina Makarenko <makarenkoirinka@gmail.com>
2022-09-07 19:25:07 +0300

    add Parameterized in mainPageListTest

--- src/test/java/ru/praktikum_services/qa_scooter/MainPageListTest.java
+++ src/test/java/ru/praktikum_services/qa_scooter/MainPageListTest.java
@@ -1,80 +1,41 @@
 package ru.praktikum_services.qa_scooter;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class MainPageListTest extends BaseUITest {
+    private final int index;
+    private final String expectedText;
 
-    @Test
-    public void ShouldBeOpenTextHeading0() {
-        MainPageList mainPageList = new MainPageList(webDriver);
-        mainPageList.clickCookieButton();
-        mainPageList.clickQuestionOfHeading(0);
-        String actualText0 = mainPageList.getTextUnderQuestionOfHeading(0);
-        assertEquals("Сутки — 400 рублей. Оплата курьеру — наличными или картой.", actualText0);
+    public MainPageListTest(int index, String expectedText) {
+        this.index = index;
+        this.expectedText = expectedText;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getOrderData() {
+        return new Object[][]{
+                {0, "Сутки — 400 рублей. Оплата курьеру — наличными или картой."},
+                {1, "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим."},
+                {2, "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30."},
+                {3, "Только начиная с завтрашнего дня. Но скоро станем расторопнее."},
+                {4, "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010."},
+                {5, "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится."},
+                {6, "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои."},
+                {7, "Да, обязательно. Всем самокатов! И Москве, и Московской области."},
+        };
     }
 
     @Test
-    public void ShouldBeOpenTextHeading1() {
+    public void ShouldBeOpenTextHeading() {
         MainPageList mainPageList = new MainPageList(webDriver);
         mainPageList.clickCookieButton();
-        mainPageList.clickQuestionOfHeading(1);
-        String actualText1 = mainPageList.getTextUnderQuestionOfHeading(1);
-        assertEquals("Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.", actualText1);
-    }
-
-    @Test
-    public void ShouldBeOpenTextHeading2() {
-        MainPageList mainPageList = new MainPageList(webDriver);
-        mainPageList.clickCookieButton();
-        mainPageList.clickQuestionOfHeading(2);
-        String actualText2 = mainPageList.getTextUnderQuestionOfHeading(2);
-        assertEquals("Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.", actualText2);
-    }
-
-    @Test
-    public void ShouldBeOpenTextHeading3() {
-        MainPageList mainPageList = new MainPageList(webDriver);
-        mainPageList.clickCookieButton();
-        mainPageList.clickQuestionOfHeading(3);
-        String actualText3 = mainPageList.getTextUnderQuestionOfHeading(3);
-        assertEquals("Только начиная с завтрашнего дня. Но скоро станем расторопнее.", actualText3);
-    }
-
-    @Test
-    public void ShouldBeOpenTextHeading4() {
-        MainPageList mainPageList = new MainPageList(webDriver);
-        mainPageList.clickCookieButton();
-        mainPageList.clickQuestionOfHeading(4);
-        String actualText4 = mainPageList.getTextUnderQuestionOfHeading(4);
-        assertEquals("Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.", actualText4);
-    }
-
-    @Test
-    public void ShouldBeOpenTextHeading5() {
-        MainPageList mainPageList = new MainPageList(webDriver);
-        mainPageList.clickCookieButton();
-        mainPageList.clickQuestionOfHeading(5);
-        String actualText5 = mainPageList.getTextUnderQuestionOfHeading(5);
-        assertEquals("Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.", actualText5);
-    }
-
-    @Test
-    public void ShouldBeOpenTextHeading6() {
-        MainPageList mainPageList = new MainPageList(webDriver);
-        mainPageList.clickCookieButton();
-        mainPageList.clickQuestionOfHeading(6);
-        String actualText6 = mainPageList.getTextUnderQuestionOfHeading(6);
-        assertEquals("Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.", actualText6);
-    }
-
-    @Test
-    public void ShouldBeOpenTextHeading7() {
-        MainPageList mainPageList = new MainPageList(webDriver);
-        mainPageList.clickCookieButton();
-        mainPageList.clickQuestionOfHeading(7);
-        String actualText7 = mainPageList.getTextUnderQuestionOfHeading(7);
-        assertEquals("Да, обязательно. Всем самокатов! И Москве, и Московской области.", actualText7);
+        mainPageList.clickQuestionOfHeading(index);
+        String actualText = mainPageList.getTextUnderQuestionOfHeading(index);
+        assertEquals(expectedText, actualText);
     }
 }
